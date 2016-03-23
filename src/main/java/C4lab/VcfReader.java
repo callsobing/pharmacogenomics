5f7277dba683d7e161564d40a13106d7f19b66f2
b02611002 <b02611002@ntu.edu.tw>
2016-03-23 22:41:57 +0800

    第三題：Modified vcfReader to get all variants with rsID
    將本來print出所有variants的迴圈註解起來，改print出所有有rsID的variants

--- src/main/java/C4lab/VcfReader.java
+++ src/main/java/C4lab/VcfReader.java
@@ -11,7 +11,7 @@ public class VcfReader
 {
     public static void main( String[] args ) throws IOException {
         VCFCodec vcfCodec = new VCFCodec();
-        final String vcfPath = "/Users/yat/vcf/deafness/A0087Y_09182015_bwamem.filtered.haplotype.SnpIndel.vcf";
+        final String vcfPath = "/Users/zhouyuda/Desktop/c4lab/c4Lab_pharmacogenomics/A0087Y_09182015_bwamem.filtered.haplotype.SnpIndel.vcf";
 
         BufferedReader schemaReader = new BufferedReader(new FileReader(vcfPath));
 
@@ -27,10 +27,22 @@ public class VcfReader
             vcfCodec.readActualHeader(new LineIteratorImpl(LineReaderUtil.fromStringReader(
                     new StringReader(headerLine), LineReaderUtil.LineReaderOption.SYNCHRONOUS)));
 
+
+            // print出所有variants
+//            if(!line.startsWith("#")) {
+//                vctx = vcfCodec.decode(line);
+//                System.out.println("ref: "+vctx.getReference() + " alt:" + vctx.getAlternateAlleles().get(0) + " GT: " +
+//                        vctx.getGenotype(vctx.getSampleNamesOrderedByName().get(0)).getGenotypeString() );
+//            }
+
+
+            // print出所有有rsID的variants
             if(!line.startsWith("#")) {
                 vctx = vcfCodec.decode(line);
-                System.out.println("ref: "+vctx.getReference() + " alt:" + vctx.getAlternateAlleles().get(0) + " GT: " +
-                        vctx.getGenotype(vctx.getSampleNamesOrderedByName().get(0)).getGenotypeString() );
+                if(!(vctx.getID()==".")) {
+                    System.out.println("rsID: " + vctx.getID() + "\t\t ref: " + vctx.getReference() + " alt:" + vctx.getAlternateAlleles().get(0) + " GT: " +
+                            vctx.getGenotype(vctx.getSampleNamesOrderedByName().get(0)).getGenotypeString());
+                }
             }
         }
     }
