12349ffac23f84de5c3dc0ce6cdc5299086c7b05
b02611002 <b02611002@ntu.edu.tw>
2016-03-24 13:20:13 +0800

    第三題：Modified vcfReader to get all variants with rsID
    將本來print出所有variants的迴圈註解起來，改print出所有有rsID的variants

---
+++ src/main/java/C4lab/VcfReaderYuta.java
@@ -0,0 +1,41 @@
+package C4lab;
+
+import htsjdk.tribble.readers.LineIteratorImpl;
+import htsjdk.tribble.readers.LineReaderUtil;
+import htsjdk.variant.variantcontext.VariantContext;
+import htsjdk.variant.vcf.VCFCodec;
+import java.io.*;
+
+
+public class VcfReaderYuta
+{
+    public static void main( String[] args ) throws IOException {
+        VCFCodec vcfCodec = new VCFCodec();
+        final String vcfPath = "/Users/zhouyuda/Desktop/c4lab/c4Lab_pharmacogenomics/A0087Y_09182015_bwamem.filtered.haplotype.SnpIndel.vcf";
+
+        BufferedReader schemaReader = new BufferedReader(new FileReader(vcfPath));
+
+        String line;
+        String headerLine = "";
+        VariantContext vctx;
+
+        while ((line = schemaReader.readLine()) != null) {
+            if(line.startsWith("#")) {
+                headerLine = headerLine.concat(line).concat("\n");
+                continue;
+            }
+            vcfCodec.readActualHeader(new LineIteratorImpl(LineReaderUtil.fromStringReader(
+                    new StringReader(headerLine), LineReaderUtil.LineReaderOption.SYNCHRONOUS)));
+
+
+            // print出所有有rsID的variants
+            if(!line.startsWith("#")) {
+                vctx = vcfCodec.decode(line);
+                if(!(vctx.getID()==".")) {
+                    System.out.println("rsID: " + vctx.getID() + "\t\t ref: " + vctx.getReference() + " alt:" + vctx.getAlternateAlleles().get(0) + " GT: " +
+                            vctx.getGenotype(vctx.getSampleNamesOrderedByName().get(0)).getGenotypeString());
+                }
+            }
+        }
+    }
+}
