cdfb3b2bdbdc5651ff9019b115096c0687d2652e
b02611002 <b02611002@ntu.edu.tw>
2016-04-05 09:50:50 +0800

    VcfReaderYuta更新： 將讀檔及對VariantContext的操作改以mathod實作，主程式只負責呼叫

--- src/main/java/C4lab/VcfReaderYuta.java
+++ src/main/java/C4lab/VcfReaderYuta.java
@@ -7,112 +7,142 @@ import htsjdk.variant.vcf.VCFCodec;
 import java.io.*;
 
 
-public class VcfReaderYuta
-{
-    public static void main( String[] args ) throws IOException {
-        VCFCodec vcfCodec = new VCFCodec();
+public class VcfReaderYuta {
+
+    //main class盡量簡化，以方便之後的使用彈性
+    public static void main(String[] args) throws IOException {
         final String vcfPath = args[0];
 
-        BufferedReader schemaReader = new BufferedReader(new FileReader(vcfPath));
-
-        String line;
-        String headerLine = "";
-        VariantContext vctx;
-
-        String[] rsIDs = {
+        //rsID
+        String[] rsIDList = {
                 "rs587638290",
                 "rs587736341",
                 "rs534965407",
-                "s9609649",
+                "rs9609649",
                 "rs5845047",
                 "rs80690",
                 "rs137506",
                 "rs138355780",
         };
-        int rsIDsLength = rsIDs.length;
-        int[] rsIDCount = new int[rsIDsLength];
 
-
-
-
-            while ((line = schemaReader.readLine()) != null) {
-                if (line.startsWith("#")) {
-                    headerLine = headerLine.concat(line).concat("\n");
-                    continue;
-                }
-                vcfCodec.readActualHeader(new LineIteratorImpl(LineReaderUtil.fromStringReader(
-                        new StringReader(headerLine), LineReaderUtil.LineReaderOption.SYNCHRONOUS)));
-
-                for (int i=0; i<rsIDsLength; i++) {
-                    //
-                    if (!line.startsWith("#")) {
-                        vctx = vcfCodec.decode(line);
-                        if (vctx.getID().equals(rsIDs[i])) {
-                            rsIDCount[i]++;
-//                            System.out.println(
-//                                    "|rsIDCount: " + rsIDCount[i]
-//                                            + "| |rsID: " + vctx.getID()
-//                                            + "| |POS: " + vctx.getEnd()
-//                                            + "| |ref: " + vctx.getReference()
-//                                            + "| |alt(getAlternateAlleles): " + vctx.getAlternateAlleles().get(0)
-//                                            + "| |GT: " + vctx.getGenotype(vctx.getSampleNamesOrderedByName().get(0)).getGenotypeString()
-//                                            + "|");
-                        }
-                    }
-                }
-                // print出所有有 rsID 的 variants：
-//            if(!line.startsWith("#")) {
-//                vctx = vcfCodec.decode(line);
-//                if(!(vctx.getID()==".")) {
-//                    System.out.println(
-//                               "|rsID: " + vctx.getID()
-//                            +"| |ref: " + vctx.getReference()
-//                            +"| |alt(getAlternateAlleles): " + vctx.getAlternateAlleles().get(0)
-//                            +"| |GT: " + vctx.getGenotype(vctx.getSampleNamesOrderedByName().get(0)).getGenotypeString()
-//                            +"| |"
-//                    );
-//                }
-//            }
-
-                // print出所有長度大於 100 的 alt allele：
-//            if (!line.startsWith("#")) {
-//                vctx = vcfCodec.decode(line);
-//                if (vctx.getAlternateAlleles().get(0).length() > 100)
-//                    System.out.println(
-//                            " |length: " + vctx.getAlternateAlleles().get(0).length() + " |" +
-//                            " |ref: " + vctx.getReference() + " |" +
-//                            " \n|alt:" + vctx.getAlternateAlleles().get(0) + " |" +
-//                            " \n|GT: " + vctx.getGenotype(vctx.getSampleNamesOrderedByName().get(0)).getGenotypeString() + " |"
-//                    );
-//            }
-
-
-                // 修改變數rsIDtarget以搜尋特定rsID
-//            if(!line.startsWith("#")) {
-//                vctx = vcfCodec.decode(line);
-//                if(vctx.getID().equals(rsIDtarget)) {
-//                    System.out.println(
-//                               "|rsID: " + vctx.getID()
-//                            +"| |POS: " + vctx.getEnd()
-//                            +"| |ref: " + vctx.getReference()
-//                            +"| |alt(getAlternateAlleles): " + vctx.getAlternateAlleles().get(0)
-//                            +"| |GT: " + vctx.getGenotype(vctx.getSampleNamesOrderedByName().get(0)).getGenotypeString()
-//                            +"|"
-//                    );
-//                }
-//            }
-            }
-
-        for (int i=0; i<rsIDsLength;i++){
-            System.out.println("Allele Frequency of " + rsIDs[i] + ": " + rsIDCount[i]/5008.0);
-        }
-
-//        System.out.println(searchWithRSID("rs71507461"));
+        //
+        printAltAllelesLongerThen(100, vcfPath);
+        printAllAllelsWithRSID(vcfPath);
+        printRSIDequals(rsIDList, vcfPath);
 
     }
 
-    public static String searchWithRSID(String rsIDtarget){
-        return rsIDtarget.concat(" inputted");
+    //以mathod來實作不同功能（包含讀檔程序）
+    public static void printAltAllelesLongerThen(int length, String vcfPath) throws IOException {
+
+        //讀檔前置作業、參數宣告
+        BufferedReader schemaReader = new BufferedReader(new FileReader(vcfPath));
+        VCFCodec vcfCodec = new VCFCodec();
+        String line;
+        String headerLine = "";
+        VariantContext vctx;
+
+        //單行讀檔
+        while ((line = schemaReader.readLine()) != null) {
+
+            //分離Information field
+            if (line.startsWith("#")) {
+                headerLine = headerLine.concat(line).concat("\n");
+                continue;
+            }
+            vcfCodec.readActualHeader(new LineIteratorImpl(LineReaderUtil.fromStringReader(
+                    new StringReader(headerLine), LineReaderUtil.LineReaderOption.SYNCHRONOUS)));
+
+            //print 出 AltAlleles 長度超過100者
+            if (!line.startsWith("#")) {
+                vctx = vcfCodec.decode(line);
+                if (vctx.getAlternateAlleles().get(0).length() > length) {
+                    System.out.println(
+                            "rsID: " + vctx.getID() +
+                                    " length: " + vctx.getAlternateAlleles().get(0).length() +
+                                    " ref: " + vctx.getReference() +
+                                    " alt:" + vctx.getAlternateAlleles().get(0) +
+                                    " GT: " + vctx.getGenotype(vctx.getSampleNamesOrderedByName().get(0)).getGenotypeString());
+                }
+            }
+        }
+    }
+
+    //以rsID搜尋variants
+    public static void printRSIDequals(String[] rsIDList, String vcfPath) throws IOException {
+
+        //讀檔前置作業、參數宣告
+        BufferedReader schemaReader = new BufferedReader(new FileReader(vcfPath));
+        VCFCodec vcfCodec = new VCFCodec();
+        String line;
+        String headerLine = "";
+        VariantContext vctx;
+        int[] rsIDCount = new int[rsIDList.length];
+
+        //單行讀檔迴圈
+        while ((line = schemaReader.readLine()) != null) {
+
+            //分離Information field
+            if (line.startsWith("#")) {
+                headerLine = headerLine.concat(line).concat("\n");
+                continue;
+            }
+            vcfCodec.readActualHeader(new LineIteratorImpl(LineReaderUtil.fromStringReader(
+                    new StringReader(headerLine), LineReaderUtil.LineReaderOption.SYNCHRONOUS)));
+
+            for (int i=0; i<rsIDList.length; i++) {
+                if (!line.startsWith("#")) {
+                    vctx = vcfCodec.decode(line);
+                    if (vctx.getID().equals(rsIDList[i])) {
+                        rsIDCount[i]++;
+                        System.out.println(
+                                "|rsIDCount: " + rsIDCount[i]
+                                        + "| |rsID: " + vctx.getID()
+                                        + "| |POS: " + vctx.getEnd()
+                                        + "| |ref: " + vctx.getReference()
+                                        + "| |alt(getAlternateAlleles): " + vctx.getAlternateAlleles().get(0)
+                                        + "| |GT: " + vctx.getGenotype(vctx.getSampleNamesOrderedByName().get(0)).getGenotypeString()
+                                        + "|");
+                    }
+                }
+            }
+        }
+    }
+
+    //print出所有擁有rsID的variants：
+    public static void printAllAllelsWithRSID(String vcfPath) throws IOException {
+
+        //讀檔前置作業、參數宣告
+        BufferedReader schemaReader = new BufferedReader(new FileReader(vcfPath));
+        VCFCodec vcfCodec = new VCFCodec();
+        String line;
+        String headerLine = "";
+        VariantContext vctx;
+
+        //單行讀檔
+        while ((line = schemaReader.readLine()) != null) {
+
+            //分離Information field
+            if (line.startsWith("#")) {
+                headerLine = headerLine.concat(line).concat("\n");
+                continue;
+            }
+            vcfCodec.readActualHeader(new LineIteratorImpl(LineReaderUtil.fromStringReader(
+                    new StringReader(headerLine), LineReaderUtil.LineReaderOption.SYNCHRONOUS)));
+
+            if (!line.startsWith("#")) {
+                vctx = vcfCodec.decode(line);
+                if (!(vctx.getID().equals("."))) {
+                    System.out.println(
+                            "|rsID: " + vctx.getID()
+                                    + "| |ref: " + vctx.getReference()
+                                    + "| |alt(getAlternateAlleles): " + vctx.getAlternateAlleles().get(0)
+                                    + "| |GT: " + vctx.getGenotype(vctx.getSampleNamesOrderedByName().get(0)).getGenotypeString()
+                                    + "| |"
+                    );
+                }
+            }
+        }
     }
 
 }
