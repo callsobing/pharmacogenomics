93860b6b0ffdeb526a8d9950d4bd7b481ae9b2f0
b02611002 <b02611002@ntu.edu.tw>
2016-04-07 02:48:26 +0800

    VcfReaderYuta更新：增加AFComparison方法。
    int AFComparison(String vcfPath) ：讀vcf檔，找出SAS_AF與EAS_AF皆大於AF的variants，並回報符合條件的variants個數(int)

--- src/main/java/C4lab/VcfReaderYuta.java
+++ src/main/java/C4lab/VcfReaderYuta.java
@@ -4,12 +4,14 @@ import htsjdk.tribble.readers.LineIteratorImpl;
 import htsjdk.tribble.readers.LineReaderUtil;
 import htsjdk.variant.variantcontext.VariantContext;
 import htsjdk.variant.vcf.VCFCodec;
+
 import java.io.*;
+import java.util.ArrayList;
 
 
 public class VcfReaderYuta {
 
-    //main class盡量簡化，以方便之後的使用彈性
+    /* main class盡量簡化，以方便之後的使用彈性 */
     public static void main(String[] args) throws IOException {
         final String vcfPath = args[0];
 
@@ -25,14 +27,13 @@ public class VcfReaderYuta {
                 "rs138355780",
         };
 
-        //
-        printAltAllelesLongerThen(100, vcfPath);
-        printAllAllelsWithRSID(vcfPath);
-        printRSIDequals(rsIDList, vcfPath);
-
+//        printAltAllelesLongerThen(100, vcfPath);
+//        printAllAllelesWithRSID(vcfPath);
+//        printRSIDEquals(rsIDList, vcfPath);
+        System.out.println(AFComparison(vcfPath));
     }
 
-    //以mathod來實作不同功能（包含讀檔程序）
+    /*讀vcf檔，並print出長度超過length的variants*/
     public static void printAltAllelesLongerThen(int length, String vcfPath) throws IOException {
 
         //讀檔前置作業、參數宣告
@@ -45,7 +46,7 @@ public class VcfReaderYuta {
         //單行讀檔
         while ((line = schemaReader.readLine()) != null) {
 
-            //分離Information field
+            //分離Metadata lines
             if (line.startsWith("#")) {
                 headerLine = headerLine.concat(line).concat("\n");
                 continue;
@@ -68,8 +69,8 @@ public class VcfReaderYuta {
         }
     }
 
-    //以rsID搜尋variants
-    public static void printRSIDequals(String[] rsIDList, String vcfPath) throws IOException {
+    /*讀vcf檔，並以rsIDList搜尋variants*/
+    public static void printRSIDEquals(String[] rsIDList, String vcfPath) throws IOException {
 
         //讀檔前置作業、參數宣告
         BufferedReader schemaReader = new BufferedReader(new FileReader(vcfPath));
@@ -82,7 +83,7 @@ public class VcfReaderYuta {
         //單行讀檔迴圈
         while ((line = schemaReader.readLine()) != null) {
 
-            //分離Information field
+            //分離Metadata lines
             if (line.startsWith("#")) {
                 headerLine = headerLine.concat(line).concat("\n");
                 continue;
@@ -109,8 +110,8 @@ public class VcfReaderYuta {
         }
     }
 
-    //print出所有擁有rsID的variants：
-    public static void printAllAllelsWithRSID(String vcfPath) throws IOException {
+    /*讀vcf檔，並print出所有擁有rsID的variants*/
+    public static void printAllAllelesWithRSID(String vcfPath) throws IOException {
 
         //讀檔前置作業、參數宣告
         BufferedReader schemaReader = new BufferedReader(new FileReader(vcfPath));
@@ -122,7 +123,7 @@ public class VcfReaderYuta {
         //單行讀檔
         while ((line = schemaReader.readLine()) != null) {
 
-            //分離Information field
+            //分離Metadata lines
             if (line.startsWith("#")) {
                 headerLine = headerLine.concat(line).concat("\n");
                 continue;
@@ -132,7 +133,7 @@ public class VcfReaderYuta {
 
             if (!line.startsWith("#")) {
                 vctx = vcfCodec.decode(line);
-                if (!(vctx.getID().equals("."))) {
+                if (!(vctx.getID() == ".")) {
                     System.out.println(
                             "|rsID: " + vctx.getID()
                                     + "| |ref: " + vctx.getReference()
@@ -144,5 +145,103 @@ public class VcfReaderYuta {
             }
         }
     }
+
+    /* 讀vcf檔，找出SAS_AF與EAS_AF皆大於AF的Variants並回報符合條件的variants的數量 */
+    public static int AFComparison(String vcfPath) throws IOException{
+
+        /* 讀檔前置作業、參數宣告 */
+        BufferedReader schemaReader = new BufferedReader(new FileReader(vcfPath));
+        VCFCodec vcfCodec = new VCFCodec();
+        String line;
+        String headerLine = "";
+        VariantContext vctx;
+        int NFreqHigher = 0;
+
+        /* 一行行讀檔 */
+        while ((line = schemaReader.readLine()) != null) {
+
+            /* 先將metadata lines的部分存到headerLine */
+            if (line.startsWith("#")) {
+                headerLine = headerLine.concat(line).concat("\n");continue;
+            }
+            vcfCodec.readActualHeader(new LineIteratorImpl(LineReaderUtil.fromStringReader(
+                    new StringReader(headerLine), LineReaderUtil.LineReaderOption.SYNCHRONOUS)));
+
+
+            /* 開始對data lines(每一筆variants)的操作： */
+            if (!line.startsWith("#")) {
+                vctx = vcfCodec.decode(line);
+
+                /* [狀況I] ID中沒有分號且AF沒有逗號者 */
+                if(!vctx.getID().contains(";")) {
+                    if (!vctx.getAttributeAsString("AF", "-1.0").contains(",")) {
+                        boolean FreqHigher = ((vctx.getAttributeAsDouble("SAS_AF", -1.0) > vctx.getAttributeAsDouble("AF", -1.0))
+                                && (vctx.getAttributeAsDouble("EAS_AF", -1.0) > vctx.getAttributeAsDouble("AF", -1.0)));
+                        System.out.println(
+                                "I  "
+                                        + " \trsID: " + vctx.getID() + " \t"
+                                        + " \tAF: " + vctx.getAttributeAsDouble("AF", -1.0)
+                                        + " \tSAS_AF: " + vctx.getAttributeAsDouble("SAS_AF", -1.0)
+                                        + " \tEAS_AF=: " + vctx.getAttributeAsDouble("EAS_AF", -1.0)
+                                        + " \tHigher Frequency? " + FreqHigher
+                        );
+                        /* 找到SAS_AF與EAS_AF皆大於AF者，則計數 */
+                        if (FreqHigher) {
+                            NFreqHigher++;
+                        }
+                    }
+
+                    /* [狀況II] 若AF的值有逗號，代表有多個alt（因此有多個allele freq），在此以ArrayList的forEach處理 */
+                    else if (vctx.getAttributeAsString("AF", "-1.0").contains(",")) {
+                        ArrayList<String> AFlist = (ArrayList) vctx.getAttribute("AF", "EMPTY");
+                        ArrayList<String> SAS_AFlist = (ArrayList) vctx.getAttribute("SAS_AF", "EMPTY");
+                        ArrayList<String> EAS_AFlist = (ArrayList) vctx.getAttribute("EAS_AF", "EMPTY");
+                        for (int i = 0; i < AFlist.size(); i++) {
+                            boolean FreqHigher = ((Double.parseDouble(SAS_AFlist.get(i)) > Double.parseDouble(AFlist.get(i)))
+                                    && (Double.parseDouble(EAS_AFlist.get(i)) > Double.parseDouble(AFlist.get(i))));
+                            System.out.println(
+                                    "II  "
+                                            + " \trsID: " + vctx.getID() + " \t"
+                                            + " \tAF: " + AFlist.get(i)
+                                            + " \tSAS_AF: " + SAS_AFlist.get(i)
+                                            + " \tEAS_AF=: " + EAS_AFlist.get(i)
+                                            + " \tHigher Frequency? " + FreqHigher
+
+                            );
+                            if (FreqHigher) {
+                                NFreqHigher++;
+                            }
+                        }
+                    }
+                }
+
+                /* [狀況III] 若ID欄有分號，代表同一位置有多個基因，在此處理 */
+                if(vctx.getID().contains(";")){
+                    String[] IDs = vctx.getID().split(";");
+                    int N = IDs.length;
+                    ArrayList<String> AFlist = (ArrayList)vctx.getAttribute("AF","EMPTY");
+                    ArrayList<String> SAS_AFlist = (ArrayList)vctx.getAttribute("SAS_AF","EMPTY");
+                    ArrayList<String> EAS_AFlist = (ArrayList)vctx.getAttribute("EAS_AF","EMPTY");
+                    for (int i=0; i<AFlist.size(); i++) {
+                        boolean FreqHigher = ((Double.parseDouble(SAS_AFlist.get(i)) > Double.parseDouble(AFlist.get(i)))
+                                && (Double.parseDouble(EAS_AFlist.get(i)) > Double.parseDouble(AFlist.get(i))));
+                        System.out.println(
+                                "III  "
+                                        + " \trsID: " + IDs[i] + " \t"
+                                        + " \tAF: " + AFlist.get(i)
+                                        + " \tSAS_AF: " + SAS_AFlist.get(i)
+                                        + " \tEAS_AF=: " + EAS_AFlist.get(i)
+                                        + " \tHigher Frequency? " + FreqHigher
+                        );
+                        if (FreqHigher){
+                            NFreqHigher++;
+                        }
+                    }
+                }
+            }
+        }
+        return NFreqHigher;
+    }
 }
+
 
