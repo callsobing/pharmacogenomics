6a6c4cad6a361ee84308f038392d08e41079c8b9
junliang4ni <b02611032@ntu.edu.tw>
2016-04-13 21:36:18 +0800

    4/13 temp

--- src/main/java/C4lab/VcfReaderJunliang.java
+++ src/main/java/C4lab/VcfReaderJunliang.java
@@ -1,20 +1,21 @@
 package C4lab;
 
-/**
- * Created by NIGHT on 2016/3/24.
- */
-
 import htsjdk.tribble.readers.LineIteratorImpl;
 import htsjdk.tribble.readers.LineReaderUtil;
+import htsjdk.variant.variantcontext.Allele;
 import htsjdk.variant.variantcontext.VariantContext;
 import htsjdk.variant.vcf.VCFCodec;
 
 import javax.lang.model.type.NullType;
+import java.io.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringReader;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class VcfReaderJunliang
 {
@@ -27,10 +28,10 @@ public class VcfReaderJunliang
         String line;
         String headerLine = "";
         VariantContext vctx;
-        int SampleN=0;
+        //int SampleN=0;
         //String[] rsid={"rs587697622","rs587638290","rs587736341","rs534965407","rs9609649","rs5845047","rs80690","rs137506","rs138355780"};
         String[] cases =new String[10];
-        String[] control=new String[10];
+        Set <String> control = new HashSet<String>();
 
         while ((line = schemaReader.readLine()) != null) {
             if(line.startsWith("#")) {
@@ -44,13 +45,13 @@ public class VcfReaderJunliang
 
                 vctx = vcfCodec.decode(line);
                 String[] SampleId = vctx.getSampleNames().toString().split(",");
-                for (int i = 0; i < 20; i++) {
+                for (int i = 0; i < 10; i++) {
                     if (i < 10)
                         cases[i] = SampleId[i];
                     else
-                        control[i - 10] = SampleId[i];
+                        control.add(SampleId[i]);
                 }
-                System.out.println();
+                System.out.println(cases[5]+vctx.getGenotypes(cases[5]));
 
             }
         }
