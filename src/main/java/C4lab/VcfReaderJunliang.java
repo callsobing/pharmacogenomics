0b442c3c8d758729c3c6aba25e86006c73260bd4
junliang4ni <b02611032@ntu.edu.tw>
2016-04-17 15:11:28 +0800

    4/17  50 sampling

--- src/main/java/C4lab/VcfReaderJunliang.java
+++ src/main/java/C4lab/VcfReaderJunliang.java
@@ -14,6 +14,11 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.util.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
 public class VcfReaderJunliang
 {
     public static void main( String[] args ) throws IOException {
@@ -26,10 +31,22 @@ public class VcfReaderJunliang
         String line;
         String headerLine = "";
         VariantContext vctx;
-        int Count=0;
-        boolean AlreadyGetID=false;
-        String[] cases =new String[5];
-        String[] control =new String[5];
+        int num=50;
+        int Count[] = new int [num];
+        boolean AlreadyGetID = false;
+        String[][] cases = new String[num][5];
+        String[][] control = new String[num][5];
+
+        int rnd;
+        Integer result[][] = new Integer[num][10];
+        HashSet rndSet = new HashSet<Integer>(10);
+        for(int i = 0; i < num; i++)
+            for(int j = 0; j < 10; j++) {
+                rnd=(int)(2500*Math.random());
+                while(!rndSet.add(rnd))
+                    rnd=(int)(2500*Math.random());
+                result[i][j]=rnd;
+            }
 
         while ((line = schemaReader.readLine()) != null) {
             if(line.startsWith("#")) {
@@ -42,25 +59,29 @@ public class VcfReaderJunliang
             if(!line.startsWith("#")) {
                 vctx = vcfCodec.decode(line);
                 if(!AlreadyGetID) {
-                    for (int i = 0; i < 10; i++) {
-                        if (i < 5)
-                            cases[i] = vctx.getSampleNamesOrderedByName().get(i);
+                    for (int i = 0; i < num; i++)
+                        for(int j = 0; j < 10; j++) {
+                        if (j < 5)
+                            cases[i][j] = vctx.getSampleNamesOrderedByName().get(result[i][j]);
                         else
-                            control[i - 5] = vctx.getSampleNamesOrderedByName().get(i);
+                            control[i][j - 5] = vctx.getSampleNamesOrderedByName().get(result[i][j]);
                     }
                     AlreadyGetID=true;
                 }
 
                 boolean isAnswer = true;
                 Allele CompareAllele = vctx.getAlternateAlleles().get(0);
-                for (int j = 0; j < 5 && isAnswer; j++)
-                    isAnswer=(!(vctx.getGenotype(cases[j]).countAllele(CompareAllele)==0))&&(vctx.getGenotype(control[j]).countAllele(CompareAllele)==0);
-                if(isAnswer) {
-                    Count=Count+1;
-                    System.out.println(vctx.getID()+"\t"+Count);
-                }
+                for(int i = 0; i < num; i++)
+                    for (int j = 0; j < 5 && isAnswer; j++) {
+                        isAnswer=(!(vctx.getGenotype(cases[i][j]).countAllele(CompareAllele)==0))&&(vctx.getGenotype(control[i][j]).countAllele(CompareAllele)==0);
+                        if(isAnswer) {
+                            Count[i]=Count[i]+1;
+                        }
+                    }
             }
         }
+        for(int i = 0; i < num; i++)
+            System.out.println("sampling"+i+":"+Count[i]);
         long end = System.currentTimeMillis( );
         System.out.println("time cost : " + (end - start) +"ms");
     }
